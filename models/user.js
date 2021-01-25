f534af63f12a7a89e70180121b515408dc7a49d0
basitullah97 <basitullah@technomites.com>
2021-01-25 15:08:58 +0500

    updating git

--- models/user.js
+++ models/user.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "please enter email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, "please enter password"],
+    minlength: [6, "password should atleast be 6 chracters long"],
   },
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);

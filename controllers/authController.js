e6538e4eaceed92e623e357cede27170714a4466
basitullah97 <basitullah@technomites.com>
2021-04-10 13:14:36 +0500

    login & signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,13 +34,13 @@ const createToken = (id) => {
   return jwt.sign({ id }, "my secret key?", { expiresIn: maxAge });
 };
 
-module.exports.signup_get = (req, res) => {
-  res.render("signup");
-};
+// module.exports.signup_get = (req, res) => {
+//   res.render("signup");
+// };
 
-module.exports.login_get = (req, res) => {
-  res.render("login");
-};
+// module.exports.login_get = (req, res) => {
+//   res.render("login");
+// };
 
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
@@ -48,8 +48,14 @@ module.exports.signup_post = async (req, res) => {
   try {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
-    res.cookie("jwtToken", token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(201).json({ user: user._id });
+    // res.cookie("jwtToken", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({
+      userId: user._id,
+      userEmail: user.email,
+      jwtToken: token,
+      jwtToken: token,
+      message: "Account Created",
+    });
   } catch (err) {
     const error = handleError(err);
     res.status(400).json({ error });
@@ -61,8 +67,13 @@ module.exports.login_post = async (req, res) => {
   try {
     const user = await User.login(email, password);
     const token = createToken(user._id);
-    res.cookie("jwtToken", token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(200).json({ user: user._id });
+    // res.cookie("jwtToken", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({
+      userId: user._id,
+      userEmail: user.email,
+      jwtToken: token,
+      message: "Login successful",
+    });
   } catch (err) {
     const error = handleError(err);
     res.status(400).json({ error });
